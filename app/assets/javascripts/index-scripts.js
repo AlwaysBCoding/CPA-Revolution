44d6e16b145766c8f144b50dc919d6ea3fcd7bd9
Jordan Leigh <JDLeigh10@gmail.com>
2012-08-12 03:06:15 -0500

    Changed the navbar and updated the sections show page

--- app/assets/javascripts/index-scripts.js
+++ app/assets/javascripts/index-scripts.js
@@ -1,5 +1,6 @@
 $(function() {
 	$(".section img").hover(function() {
+		$("#contentTable").html("");
 		$(this).addClass("hover-highlight");
 		$(".section img:not('.hover-highlight')").stop().animate({ opacity: .4 }, 300);
 		
@@ -12,10 +13,12 @@ $(function() {
 			case "BEC": sectionName = "Business Envirnonments and Concepts"; break;
 		}
 		$("#sectionName").html(sectionName);
+		$("#contentTable").html($("table." + sectionCode).html());
 		
 	}, function() {
 		$(this).removeClass("hover-highlight");
 		$(".section img").stop().animate( { opacity: 1 }, 300);
 		$("#sectionName").html("");
+		$("#contentTable").html("");
 	});
 });
