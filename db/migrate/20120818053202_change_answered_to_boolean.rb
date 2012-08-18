b4cd5410154568a2b95ba07689a948056b9bb78c
Jordan Leigh <JDLeigh10@gmail.com>
2012-08-18 15:33:37 -0500

    Fixed a stupid mistake, it's REMOVE_column, not DELETE_column

--- db/migrate/20120818053202_change_answered_to_boolean.rb
+++ db/migrate/20120818053202_change_answered_to_boolean.rb
@@ -1,6 +1,6 @@
 class ChangeAnsweredToBoolean < ActiveRecord::Migration
   def up
-    delete_column :user_test_questions, :answered
+    remove_column :user_test_questions, :answered
     add_column :user_test_questions, :answered_correct, :boolean
   end
 
