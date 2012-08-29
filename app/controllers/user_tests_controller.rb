29b2ed8c9467253ec454fa9f377f16083813270a
Jordan Leigh <JDLeigh10@gmail.com>
2012-08-28 22:43:06 -0500

    Redid some CSS for the Sandbox

--- app/controllers/user_tests_controller.rb
+++ app/controllers/user_tests_controller.rb
@@ -35,8 +35,9 @@ class UserTestsController < ApplicationController
     # If the request has a ?question= at the end of it, then render the JSON data of the question
     if params[:question].present?
     @question = UserTestQuestion.where(:user_test_id => session[:user_test_id]).offset(params[:question].to_i - 1).limit(1)[0]
-    puts @question
-    render json: JSON.pretty_generate(JSON.parse(@question.to_json(:include => { :question => { :methods => [:answers, :topic] } })))
+    
+    render yaml: @question.to_yaml
+    # render json: JSON.pretty_generate(JSON.parse(@question.to_json(:include => { :question => { :methods => [:answers, :topic] } })))
     end  
     
     @user_test = UserTest.find(session[:user_test_id])
