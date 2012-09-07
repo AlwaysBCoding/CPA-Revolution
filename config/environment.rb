761fe64907eadd931428d58d3322f2fc95e2ea64
Student <student@CA-iMac-06.local>
2012-09-07 15:35:56 -0500

    Turned production error messages off

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 CpaMagic::Application.initialize!
-
-class Numeric
-  def roundup(nearest)
-    self % nearest == 0 ? self : self + nearest - (self % nearest)
-  end
-end
